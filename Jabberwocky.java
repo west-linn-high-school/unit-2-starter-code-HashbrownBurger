14d60049fa15803ed0068a08a6839c9ac50a158d
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-09-06 09:15:41 -0700

    Signed-off-by: Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>

---
+++ Jabberwocky.java
@@ -0,0 +1,13 @@
+
+public class Jabberwocky
+{ 
+    public static void main(String [] args){
+     System.out.print("'Twas brillig, and the slithy toves/\n"
+     + "Did Gyre and gimbleie in he wabe:\n"
+     + "All mimsy were the \"Borograves\"/\n"
+     + "And the mome raths out grabe."
+     );
+       
+     
+    }
+}
