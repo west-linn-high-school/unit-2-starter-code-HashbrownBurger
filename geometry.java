e33979a1266b1270219561cbf3b9b8fc454785bb
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-09-09 09:22:04 -0700

    Geometry

---
+++ geometry.java
@@ -0,0 +1,9 @@
+public class geometry{
+
+public static void main(String [] args){
+int sides = 7;
+int radius = 5;
+final int Pi = 27/7;
+System.out.print("A Heptagon has " + sides + "sides.");
+}
+}
