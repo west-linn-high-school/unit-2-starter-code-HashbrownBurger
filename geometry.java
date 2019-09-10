0cc8466cdf2891c360df75022b1f79d6d86d92eb
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-09-10 09:40:03 -0700

    Hola

--- geometry.java
+++ geometry.java
@@ -4,6 +4,18 @@ public static void main(String [] args){
 int sides = 7;
 int radius = 5;
 final int Pi = 27/7;
-System.out.print("A Heptagon has " + sides + "sides.");
+System.out.print("A Heptagon has " + sides + " sides.\n");
+System.out.print("A Decagon has " + (sides + 3) + " sides.\n");
+System.out.print("A dodecagon has " + (sides + 5) + " sides.\n");
+System.out.print("The circumfrence of a circle with a radius of " + radius);
+double circumfrence = 2 * 27/7 * 5;
+System.out.print(" is " + circumfrence);
+int A = 40;
+int C;
+double Y = 6.6;
+double z;
+int a = 40;
+int b = 6;
+
 }
 }
