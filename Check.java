8646254df1a93e153da5c00a67d7de69bf001c58
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-09-16 09:24:09 -0700

    Atom

---
+++ Check.java
@@ -0,0 +1,19 @@
+java.untl.Scanner
+/**
+ * Write a description of class Check here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Check
+{
+{public static void main(String[]arg){
+ 
+ n1 = 5;
+ n2 = 3;
+ n3 = 9;
+ System.out.println(" " +(n1 + n2 + n3)/3);
+}
+}
+
+}
